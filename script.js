a7e7976ef079381059b838711bd5fcb700183b13
abhishek <57187026+anonymous-cloud@users.noreply.github.com>
2025-01-24 04:25:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+
+
+function name() {
+	const firstName =  document.getElementById("fName").value
+const lastName  =  document.getElementById("lName").value
+const phone  =  document.getElementById("phone").value
+const email  =  document.getElementById("email").value
+	
+
+	const message  = `firstName: ${firstName} Last Name :${lastName} Phone Number:${phone} Email Id: ${email}`
+	alert(message)
+}
+
+
